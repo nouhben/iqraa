0b6101470237877e1f95720b800e19f0f7a83714
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-09 02:27:10 +0100

    Added components dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iqraa/components/rounded_button.dart';
 import 'package:iqraa/constants.dart';
 import 'package:iqraa/size_config.dart';
 
@@ -66,53 +67,6 @@ class WelcomeScreen extends StatelessWidget {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  final String label;
-  final Function onPress;
-  const RoundedButton({
-    this.label,
-    this.onPress,
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: this.onPress,
-      child: Container(
-        margin: EdgeInsets.symmetric(
-          vertical: SizeConfig.defaultSize * 1.6, //16
-        ),
-        padding: EdgeInsets.symmetric(
-          vertical: SizeConfig.defaultSize * 1.6,
-          horizontal: SizeConfig.defaultSize * 3, //30 //16
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-          boxShadow: [
-            BoxShadow(
-              offset: Offset(0, 21),
-              blurRadius: 45.0,
-              spreadRadius: -12,
-              color: Color(0xff666666).withOpacity(0.35),
-              //opacity should be 35% & color same as the container
-            ),
-          ],
-        ),
-        child: Text(
-          this.label,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: SizeConfig.defaultSize * 1.6,
-            fontWeight: FontWeight.bold,
-          ),
         ),
       ),
     );
