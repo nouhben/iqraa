2290ed05df60fd1c98ce05f7c627f63a940dfff1
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-09 03:21:30 +0100

    Added book rating widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -41,31 +41,89 @@ class HomeScreen extends StatelessWidget {
               width: SizeConfig.defaultSize * 20.2,
               child: Stack(
                 children: [
-                  Container(
-                    height: SizeConfig.defaultSize * 22.1,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius:
-                          BorderRadius.circular(SizeConfig.defaultSize * 3),
-                      boxShadow: [
-                        BoxShadow(
-                          offset: Offset(0, 18),
-                          blurRadius: 43,
-                          //spreadRadius: -12.0,
-                          color: kShadowColor,
-                        ),
-                      ],
-                    ), //221
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    left: 0,
+                    child: Container(
+                      height: SizeConfig.defaultSize * 22.1,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.circular(SizeConfig.defaultSize * 3),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 18),
+                            blurRadius: 43,
+                            //spreadRadius: -12.0,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ), //221
+                    ),
                   ),
                   Image.asset(
                     'images/book-1.png',
                     width: SizeConfig.defaultSize * 15,
+                  ),
+                  Positioned(
+                    top: 35,
+                    right: 10,
+                    child: Column(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.favorite_border),
+                          onPressed: () {},
+                        ),
+                        BookRating(score: 4.9),
+                      ],
+                    ),
                   ),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BookRating extends StatelessWidget {
+  final double score;
+  const BookRating({Key key, @required this.score}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(SizeConfig.defaultSize),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(3, 7),
+            blurRadius: 20.0,
+            color: kShadowColor.withOpacity(0.5),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Icon(
+            Icons.star,
+            color: kIconColor,
+            size: SizeConfig.defaultSize * 1.5,
+          ),
+          SizedBox(height: SizeConfig.defaultSize),
+          Text(
+            this.score.toString() ?? '0',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 12,
+            ),
+          ),
+        ],
       ),
     );
   }
