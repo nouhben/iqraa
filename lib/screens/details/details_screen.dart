4693d35415463e061cd2fe3ea7a96c518f7c0301
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-09 19:07:47 +0100

    cotinue details more

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -35,59 +35,7 @@ class DetailsScreen extends StatelessWidget {
                     child: Column(
                       children: [
                         SizedBox(height: SizeConfig.screenHeight * 0.1),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    book.title,
-                                    style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),
-                                  ),
-                                  SizedBox(height: 10),
-                                  Row(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: Column(
-                                          children: [
-                                            Text(
-                                              'When earth was flat and everyone wanted to win the game of the best and people... .',
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                height: 1.5,
-                                                color: kLightBlackColor,
-                                              ),
-                                              maxLines: 5,
-                                            ),
-                                            SizedBox(height: 5),
-                                            RoundedButton(
-                                              label: 'Read',
-                                              onPress: () {},
-                                              //horizontalPadding: 16,
-                                              verticalPadding: 10.0,
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                      Column(
-                                        children: [
-                                          IconButton(icon: Icon(Icons.favorite_border), onPressed: () {}),
-                                          BookRating(score: book.ratingScore),
-                                        ],
-                                      ),
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                            Image.asset(
-                              book.image,
-                              width: 200.0,
-                            ),
-                          ],
-                        ),
+                        BookInfo(book: book),
                       ],
                     ),
                   ),
@@ -97,6 +45,72 @@ class DetailsScreen extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class BookInfo extends StatelessWidget {
+  const BookInfo({
+    Key key,
+    @required this.book,
+  }) : super(key: key);
+
+  final Book book;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                book.title,
+                style: Theme.of(context).textTheme.headline5.copyWith(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          'When earth was flat and everyone wanted to win the game of the best and people... .',
+                          style: TextStyle(
+                            fontSize: 10,
+                            height: 1.5,
+                            color: kLightBlackColor,
+                          ),
+                          maxLines: 5,
+                        ),
+                        SizedBox(height: 5),
+                        RoundedButton(
+                          label: 'Read',
+                          onPress: () {},
+                          //horizontalPadding: 16,
+                          verticalPadding: 10.0,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      IconButton(icon: Icon(Icons.favorite_border), onPressed: () {}),
+                      BookRating(score: book.ratingScore),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        Image.asset(
+          book.image,
+          width: 200.0,
+        ),
+      ],
     );
   }
 }
