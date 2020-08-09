03f10bfa57c9f1eb603d6762d5223f6d3b84f716
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-09 03:58:51 +0100

    2 side rounded btn into separat file

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -137,35 +137,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-class TowSideRoundButton extends StatelessWidget {
-  final String text;
-  final Function onPress;
-  const TowSideRoundButton({
-    Key key,
-    this.text,
-    this.onPress,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: this.onPress,
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 10),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(29.0),
-            bottomLeft: Radius.circular(29.0),
-          ),
-        ),
-        child: Text(
-          this.text ?? '',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-    );
-  }
-}
