0a9b0a60fd90367a2bfdd60f62fb0c3cc6b44d2f
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-09 04:46:43 +0100

    best of

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iqraa/constants.dart';
 
 import 'package:iqraa/models/book.dart';
 
@@ -51,6 +52,71 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                children: [
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headline4,
+                      children: [
+                        TextSpan(text: 'Best of the '),
+                        TextSpan(
+                          text: 'day',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: kBlackColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 24),
+                    height: 205,
+                    width: double.infinity,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          left: 0,
+                          bottom: 0,
+                          child: Container(
+                            height: 185,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              color: Color(0xffeaeaea).withOpacity(0.45),
+                              //
+                              borderRadius: BorderRadius.circular(29),
+                            ),
+                            padding: EdgeInsets.only(
+                              left: 24,
+                              top: 24,
+                              right: SizeConfig.screenWidth * 0.35,
+                            ),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'New yourk Time best 11th may 2020',
+                                  style: TextStyle(color: kBlackColor),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Image.asset(
+                            'images/book-3.png',
+                            width: SizeConfig.screenWidth * 0.37,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
