1727890f84593be4d0e6f48965721f48c494e44d
nouh ben kadi <benkadi.nouh@icloud.com>
2020-08-10 04:43:37 +0100

    chapter card separtion and update

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iqraa/components/rounded_button.dart';
+import 'package:iqraa/constants.dart';
 import 'package:iqraa/models/book.dart';
 import 'package:iqraa/screens/details/components/book_info.dart';
 import 'package:iqraa/screens/details/components/chapter_card.dart';
+import 'package:iqraa/screens/home/components/book_rating.dart';
 import 'package:iqraa/size_config.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -54,6 +57,100 @@ class DetailsScreen extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.headline5,
+                      children: [
+                        TextSpan(
+                          text: "You might also ",
+                        ),
+                        TextSpan(
+                          text: "like….",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        height: 180,
+                        width: double.infinity,
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Container(
+                          padding: EdgeInsets.only(left: 24, top: 24, right: 150),
+                          height: 160,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(29),
+                            color: Color(0xFFFFF8F9),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(color: kBlackColor),
+                                  children: [
+                                    //Should fetch it by the author
+                                    TextSpan(
+                                      text: "${books[2].title}\n",
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: "${books[2].author}",
+                                      style: TextStyle(color: kLightBlackColor),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                children: <Widget>[
+                                  BookRating(
+                                    score: books[2].ratingScore,
+                                  ),
+                                  SizedBox(width: 10),
+                                  Expanded(
+                                    child: RoundedButton(
+                                      label: "Read",
+                                      verticalPadding: 10,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Image.asset(
+                          books[2].image,
+                          width: 150,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
             ),
           ],
         ),
